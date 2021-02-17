007525875de1092744b6042b2b362035891e9cb1
Mika <73663569+LaHonix@users.noreply.github.com>
2021-02-17 20:18:30 +0100

    Update VAV.1.py

--- VAV.1.py
+++ VAV.1.py
@@ -20,6 +20,7 @@ import calendar
 import random
 import wikipedia 
 import subprocess
+import requests
 
 
 warnings.filterwarnings("ignore")
@@ -59,6 +60,50 @@ def wakeWord(text):
             return True
         
     return False
+def weather(text):
+    wordList = text.split()
+    for i in range(0, len(wordList)):
+        if i +3 <= len(wordList) - 1 and wordList[i].lower() == "in":
+            return wordList[i+1]
+       
+def status():
+    now = datetime.datetime.now()
+    my_date = datetime.datetime.today()
+    weekday = calendar.day_name[my_date.weekday()]
+    monthNum = now.month
+    dayNum = now.day
+    
+    month_names = ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"]
+    ordinalNumber = ["Erste", "Zweite", " Dritte", "Vierte", " Fünfte", "Sechste", "Siebte", "Achte", "Neunte", "Zehnte", "Elfte", "Zwölfte", "Dreizehnte", "Vierzehnte", "Fünfzehnte",
+    "Sechzehnte", "Siebzehnte", "Achtzehnte", "Neunzehnte", "Zwanzigste", "Einundzwanzigste", "Zweiundzwanzigste", "Dreiundzwanzigste", "Vierundzwanzigste", "Fünfundzwanzigste", "Sechsundzwanzigste",
+    "Siebenundzwanzigste", "Achtundzwanzigste", "Neunundzwanzigste", "Dreißigste","Einunddreißigste"]
+
+    if weekday == "Monday":
+        weekday = "Montag"
+    if weekday == "Tuesday":
+        weekday = "Dienstag"
+    if weekday == "Wednesday":
+        weekday = "Mittwoch"
+    if weekday == "Thursday":
+        weekday = "Donnerstag"
+    if weekday == "Friday":
+        weekday = "Freitag"
+    if weekday == "Saturday":
+        weekday = "Samsatg"
+    if weekday == "Sunday":
+        weekday = "Sonntag"
+
+    
+   
+    user_api = "989fd048fb579d6222799cb5d1e294d9"
+    complete_api_link = "https://api.openweathermap.org/data/2.5/weather?q=Neumünster&appid="+user_api
+    api_link = requests.get(complete_api_link)
+    api_data = api_link.json()
+    temp_city = ((api_data['main']['temp']) - 273.15)
+    temp_city = int(temp_city)
+    temp_city = str(temp_city)
+  
+    return "Heute ist " + weekday + " der " + ordinalNumber[dayNum -1] + " " + month_names[monthNum -1] + ". In Neumünster sind aktuell " + temp_city + " Grad."
 
 def getDate():
     now = datetime.datetime.now()
@@ -210,7 +255,24 @@ while True:
 
         if "welcher tag" and "heute" in text.lower():
             response = response + getDay()
+            
+        weatherSTR = ["grad", "wetter"]
+        for phrases in weatherSTR:
+            if phrases in text.lower():
+                wetter = weather(text)
+                wetter = str(wetter)
+                user_api = "989fd048fb579d6222799cb5d1e294d9"
+                complete_api_link = "https://api.openweathermap.org/data/2.5/weather?q="+wetter+"&appid="+user_api
+                api_link = requests.get(complete_api_link)
+                api_data = api_link.json()
+                temp_city = ((api_data['main']['temp']) - 273.15)
+                temp_city = int(temp_city)
+                temp_city = str(temp_city)
+                response = response + temp_city
 
+        if "status" in text.lower():
+            statusV = status()
+            response = response + statusV
 
         if "wer ist" in text.lower():
             person = getPerson(text)
