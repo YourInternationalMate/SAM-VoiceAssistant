c3905035c15fa757dc9f96c9fab8406e31fb62a4
Mika <73663569+YourInternationalMate@users.noreply.github.com>
2023-09-28 12:34:53 +0200

    Delete Sam directory

--- Sam/weather.py
+++
@@ -1,5 +0,0 @@
-def weather(text):      # Wetter
-    wordList = text.split()
-    for i in range(0, len(wordList)):
-        if i +3 <= len(wordList) - 1 and wordList[i].lower() == "in":
-            return wordList[i+1]
